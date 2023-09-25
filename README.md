68000e222a9e9cbd7f6a86a2b570b94af45f0d68
tuyenvan <tuyenbv.dev@gmail.com>
2023-09-25 16:27:33 +0700

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+### Description
+
+A simple portfolio template for Computer Science and Cybersecurity professionals built with React. 
+
+# npm install
+# npm start
---
+++ src/content_option.js
@@ -0,0 +1,155 @@
+const logotext = "<BVT/>";
+const meta = {
+    title: "BVT",
+    description: "I'm Bui Van Tuyen, a technologist and cybersecurity enthuist.",
+};
+
+const introdata = {
+    title: "Hi, I'm TuyenVan",
+    animated: {
+        first: "Software Engineer",
+        fourth: "Software Engineer",
+        fifth: "Software Engineer",
+    },
+    description: "Dedicated technology enthusiast with an insatiable curiosity for unraveling the complexities of our digital world.",
+    your_img_url: "https://i.ibb.co/WKfYtyV/me.jpg",
+};
+
+const dataabout = {
+    title: "Who am I?",
+    aboutme: "My name is Bui Van Tuyen, a software developer with 3 years of experience in developing web applications and desktop applications in the field of online marketing, helping users to automate repetitive activities. When building your personal brand or advertising your product on social networking sites such as Facebook, Instagram, ZALO. And have experience working with PHP, NodeJS, ReactJS, Angular.",
+};
+const worktimeline = [{
+        jobtitle: "Software Developer",
+        where: "ATP Software",
+        date: "2019-2021",
+    },
+    {
+        jobtitle: "Software Developer",
+        where: "Freelancer",
+        date: "2022-current",
+    },
+];
+
+const skills = [{
+        name: "NodeJS",
+    },
+    
+    {
+        name: "ReactJS",
+    },
+    
+    {
+        name: "JavaScript",
+    },
+    
+    {
+        name: "PHP",
+    },
+
+    {
+        name: "Angular",
+    },
+
+    {
+        name: "MongoDB",
+    },
+    {
+        name: "MySQL",
+    },
+
+    
+
+
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "buivantuyen.97@gmail.com",
+    description: "Looking forward to hearing from you!",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_8n3jkx8",
+    YOUR_TEMPLATE_ID: "template_gqg6oop",
+    YOUR_USER_ID: "cyFRGEAdfTCbo-A9X",
+};
+
+const socialprofils = {
+    github: "https://github.com/TuyenVan-dev",
+    linkedin: "https://www.linkedin.com/in/tuyen-van-56696720b/",
+    facebook: "https://www.facebook.com/TuyenVann.dev/",
+    twitter: "https://twitter.com/TuyenVan15"
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
